cd220d4fd51fc5cd28ab882b1057c6f22b1c3f2d
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-26 22:57:43 +0100

    addded the middleware func

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -8,9 +8,7 @@ const {
   deleteGoal,
 } = require("../controllers/goalController");
 
-router.get("/",getGoal);
-router.post("/",setGoal);
-router.put("/:id",updateGoal);
-router.delete("/:id",deleteGoal);
+router.route("/").get(getGoal).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal)
 
 module.exports = router;
