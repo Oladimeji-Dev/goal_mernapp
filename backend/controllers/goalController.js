d6bf97cefd9797b7b75ff1fa8a3f382c7e3c2573
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-27 00:09:16 +0100

    created the crud Controller

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,45 +1,71 @@
+const Goal = require("../models/goalModel");
+
+const asyncHandler = require("express-async-handler");
+
 //@desc   Get goals
 //@route  Get  api/goals
 //@access Private
-const getGoal = (req, res) => {
-  res.status(200).json({
-    message: "get Goals",
-  });
-};
+const getGoal = asyncHandler(async (req, res) => {
+  const goals = await Goal.find();
+  res.status(200).json(goals);
+});
 
 //@desc   Set goals
 //@route  Post  api/goals
 //@access Private
-const setGoal = (req, res) => {
-    if(!req.body.text){
-        res.status(400)
-        throw new Error("Please add all text fields")
-    }
-   
-  res.status(200).json({
-    message: "Create  Goals",
-  });
-};
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add all text fields");
+  }
 
+  const goal = await Goal.create(req.body);
+
+  res.status(200).json({
+    message: "Created Goals",
+    data: goal,
+  });
+});
 
 //@desc   Update goals
 //@route  Put  api/goals
 //@access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
+  const goalExist = await Goal.findById(req.params.id);
+
+  if (!goalExist) {
+    res.status(400);
+    throw new Error("Goal does not exit");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
   res.status(200).json({
     message: `Update Goals ${req.params.id}`,
+    data: updatedGoal
   });
-};
-
+});
 
 //@desc   Delete goals
 //@route  Delete  api/goals
 //@access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
+    const goalExist = await Goal.findById(req.params.id);
+
+  if (!goalExist) {
+    res.status(400);
+    throw new Error("Goal does not exit");
+  }
+
+  const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
+
   res.status(200).json({
     message: `Delete Goals ${req.params.id}`,
+    data: deletedGoal
   });
-};
+});
 
 module.exports = {
   getGoal,
