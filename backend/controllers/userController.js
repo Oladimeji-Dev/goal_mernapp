a7dca04959c5b90ed2e3c775e25a5cc9153e362c
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-27 01:19:31 +0100

    feat: added jwt and bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,36 @@
-//@desc   Get users
-//@route  Get  api/users
-//@access Private
-const getUsers = async (req, res) => {
+//@desc   Register user
+//@route  POST  api/users
+//@access PUBLIC
+const registerUser = async (req, res) => {
+  res.status(200).json({
+    message: "register users",
+  })
+};
 
-    res.status(200).json({
-        message: 'get users'
-    });
-  };
 
 
-  module.exports = {
-    getUsers
-  }
+//@desc   Authenticate the user
+//@route  POST  api/users/login
+//@access PUBLIC
+const loginUser = async (req, res) => {
+  res.status(200).json({
+    message: "Login users",
+  })
+};
+
+
+
+//@desc   Get user data
+//@route  GET  api/userS/ME
+//@access PUBLIC
+const getMe = async (req, res) => {
+  res.status(200).json({
+    message: "get users",
+  })
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe
+};
