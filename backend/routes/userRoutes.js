89297c42156b8fa52fcb341b89c5ff290722419c
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-27 01:18:27 +0100

    feat: created the route for the user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userController");
 
-router.get('/').getUsers()
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/me", getMe);
 
-const { getUsers } = require("../controllers/userController");
+module.exports = router;
